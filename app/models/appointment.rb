f50189bae4b3452a10d061b429a7648db022c870
SengulolanCem <c.sengul@etu.edu.tr>
2018-07-07 12:13:37 +0300

    Validator of End Time Added

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -7,5 +7,18 @@ class Appointment < ApplicationRecord
 	  else
 	    self.all
 	  end
+		end
+
+	validate :end_after_start
+	validates :start_time, :end_time, :presence => true
+
+	private
+	def end_after_start
+		return if end_time.blank? || start_time.blank?
+
+		if end_time < start_time || end_time == start_time
+			errors.add(:end_time, "must be after the start time")
+		end
 	end
+
 end
